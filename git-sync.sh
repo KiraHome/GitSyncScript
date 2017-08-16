f4c5b53755fa8c034b4c75ba62f32d6a69d2c05b
Daniel Preszl <daniel.preszl@ge.com>
2017-08-16 12:56:54 +0200

    Add colouring and awk END part that checkouts the starting branch

--- git-sync.sh
+++ git-sync.sh
@@ -2,23 +2,26 @@
 #
 # One line version:
 #
-# git branch | awk 'BEGIN {FS=""; cmd = "git branch | wc -l"; cmd | getline branchCount;} {print "Number of branches: " NR "/" branchCount; print $0; for(i=1; i<=NF; ++i){if($i!=" " && $i!="*")var=var""$i;}} {	if("$(git st|grep \"Changes not staged for commit:\")") {system("git stash"); wasStashed=1;} print var;	system("git co " var); system("git pull"); if(wasStashed==1){system("git stash pop"); wasStashed=0;} var="";}'
+# git branch | awk 'BEGIN {FS=""; cmd = "git branch | wc -l"; cmd | getline branchCount;base_branch = "master";} {print "\033[92mNumber of branches: " NR "/" branchCount;print "\033[94m" $0 "\033[39m"; for(i=1; i<=NF; ++i){if($i!=" " && $i!="*")var=var""$i;}} {	if("$(git st|grep \"Changes not staged for commit:\")") {system("git stash"); wasStashed=1;} print var;	system("git co " var); system("git pull"); if(wasStashed==1){system("git stash pop"); wasStashed=0;} var="";} END{wasStashed=0;print "\033[92mReturn to original branch: " base_branch "\033[39m"; if("$(git st|grep \"Changes not staged for commit:\")"){system("git stash");wasStashed=1;} system("git co " base_branch); if(wasStashed==1){system("git stash pop");wasStashed=0;}}'
 #
 # Beautified version:
 git branch | awk 'BEGIN {
 	FS=""
 	cmd = "git branch | wc -l";
-	cmd | getline branchCount
+	cmd | getline branchCount;
+	base_branch = "master";
 }
 
 {
-	print "Number of branches: " NR "/" branchCount;
-	print $0
+	print "\033[92mNumber of branches: " NR "/" branchCount;
+	print "\033[94m" $0 "\033[39m";
 	for(i=1; i<=NF; ++i)
 	{
 		if($i!=" " && $i!="*")
 			var=var""$i;
 	}
+	if(NR == 1)
+		base_branch = var;
 }
 
 {
@@ -36,4 +39,21 @@ git branch | awk 'BEGIN {
 		wasStashed=0;
 	}
 	var="";
+}
+
+END {
+	#return to base branch and pop the stack content that belongs to it
+	wasStashed=0;
+	print "\033[92mReturn to original branch: " base_branch "\033[39m";  
+	if("$(git st|grep \"Changes not staged for commit:\")") {
+		system("git stash");
+		wasStashed=1;
+	}
+	
+	system("git co " base_branch);
+	
+	if(wasStashed==1) {
+		system("git stash pop");
+		wasStashed=0;
+	}
 }'
