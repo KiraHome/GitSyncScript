c9a2a413b4fbe94b0062879ad9b94472019178ac
Daniel Preszl <daniel.preszl@ge.com>
2017-08-17 09:15:40 +0200

    Add different branch colouring

--- git-sync.sh
+++ git-sync.sh
@@ -2,7 +2,7 @@
 #
 # One line version:
 #
-# git branch | awk 'BEGIN {FS=""; cmd = "git branch | wc -l"; cmd | getline branchCount;base_branch = "master";} {print "\033[92mNumber of branches: " NR "/" branchCount;print "\033[94m" $0 "\033[39m"; for(i=1; i<=NF; ++i){if($i!=" " && $i!="*")var=var""$i;}} {	if("$(git st|grep \"Changes not staged for commit:\")") {system("git stash"); wasStashed=1;} print var;	system("git co " var); system("git pull"); if(wasStashed==1){system("git stash pop"); wasStashed=0;} var="";} END{wasStashed=0;print "\033[92mReturn to original branch: " base_branch "\033[39m"; if("$(git st|grep \"Changes not staged for commit:\")"){system("git stash");wasStashed=1;} system("git co " base_branch); if(wasStashed==1){system("git stash pop");wasStashed=0;}}'
+# git branch | awk 'BEGIN {FS=""; cmd = "git branch | wc -l"; cmd | getline branchCount;base_branch = "master";} {print "\033[92mNumber of branches: " NR "/" branchCount;print "\033[94m" $0 "\033[39m"; for(i=1; i<=NF; ++i){if($i!=" " && $i!="*")var=var""$i;}} {	if("$(git st|grep \"Changes not staged for commit:\")") {system("git stash"); wasStashed=1;} print var;	system("git co " var); system("git pull"); if(wasStashed==1){system("git stash pop"); wasStashed=0;} var="";} END{wasStashed=0;print "\033[92mReturn to original branch: \033[94m" base_branch "\033[39m"; if("$(git st|grep \"Changes not staged for commit:\")"){system("git stash");wasStashed=1;} system("git co " base_branch); if(wasStashed==1){system("git stash pop");wasStashed=0;}}'
 #
 # Beautified version:
 git branch | awk 'BEGIN {
@@ -44,7 +44,7 @@ git branch | awk 'BEGIN {
 END {
 	#return to base branch and pop the stack content that belongs to it
 	wasStashed=0;
-	print "\033[92mReturn to original branch: " base_branch "\033[39m";  
+	print "\033[92mReturn to original branch: " "\033[94m" base_branch "\033[39m";  
 	if("$(git st|grep \"Changes not staged for commit:\")") {
 		system("git stash");
 		wasStashed=1;
