967d063a79d4891093506e0f447ac2dad4320365
Daniel Preszl <daniel.preszl@ge.com>
2017-08-18 08:29:47 +0200

    Fix returning to starting branch issue

--- git-sync.sh
+++ git-sync.sh
@@ -2,7 +2,7 @@
 #
 # One line version:
 #
-# git branch | awk 'BEGIN {FS=""; cmd = "git branch | wc -l"; cmd | getline branchCount;base_branch = "master";} {print "\033[92mNumber of branches: " NR "/" branchCount;print "\033[94m" $0 "\033[39m"; for(i=1; i<=NF; ++i){if($i!=" " && $i!="*")var=var""$i;}} {	if("$(git st|grep \"Changes not staged for commit:\")") {system("git stash"); wasStashed=1;} print var;	system("git co " var); system("git pull"); if(wasStashed==1){system("git stash pop"); wasStashed=0;} var="";} END{wasStashed=0;print "\033[92mReturn to original branch: \033[94m" base_branch "\033[39m"; if("$(git st|grep \"Changes not staged for commit:\")"){system("git stash");wasStashed=1;} system("git co " base_branch); if(wasStashed==1){system("git stash pop");wasStashed=0;}}'
+# git branch | awk 'BEGIN {FS=""; cmd = "git branch | wc -l"; cmd | getline branchCount;base_branch = "master";wasBaseChecked = 0;} {print "\033[92mNumber of branches: " NR "/" branchCount;print "\033[94m" $0 "\033[39m"; for(i=1; i<=NF; ++i){if($i!=" " && $i!="*")var=var""$i;}if($1=="*"&&wasBaseChecked==0){wasBaseChecked=1;base_branch=var;}} {if("$(git st|grep \"Changes not staged for commit:\")") {system("git stash"); wasStashed=1;} print var;	system("git co " var); system("git pull"); if(wasStashed==1){system("git stash pop"); wasStashed=0;} var="";} END{wasStashed=0;print "\033[92mReturn to original branch: \033[94m" base_branch "\033[39m"; if("$(git st|grep \"Changes not staged for commit:\")"){system("git stash");wasStashed=1;} system("git co " base_branch); if(wasStashed==1){system("git stash pop");wasStashed=0;}}'
 #
 # Beautified version:
 git branch | awk 'BEGIN {
@@ -10,6 +10,7 @@ git branch | awk 'BEGIN {
 	cmd = "git branch | wc -l";
 	cmd | getline branchCount;
 	base_branch = "master";
+	wasBaseChecked = 0;
 }
 
 {
@@ -20,8 +21,10 @@ git branch | awk 'BEGIN {
 		if($i!=" " && $i!="*")
 			var=var""$i;
 	}
-	if(NR == 1)
+	if($1 == "*" && wasBaseChecked == 0) {
+		wasBaseChecked = 1;
 		base_branch = var;
+	}
 }
 
 {
